4664d28b0e2cbfada0dd82264b73ce7785a606be
jojoldu <jojoldu@gmail.com>
2021-08-16 15:12:25 +0900

    entity 모듈에 typeORM 적용

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
 import { EntityService } from '@app/entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { User } from '@app/entity/user/User';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'test',
+      password: 'test',
+      database: 'test',
+      entities: [User],
+      synchronize: true,
+      namingStrategy: new SnakeNamingStrategy(),
+    }),
+  ],
   controllers: [ApiController],
   providers: [ApiService, EntityService],
 })
