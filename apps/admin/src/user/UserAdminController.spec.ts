385276e470a24335b126cdcd01af00ac9f7fb532
jojoldu <jojoldu@gmail.com>
2021-08-19 08:24:47 +0900

    단위 테스트 코드 추가

--- apps/admin/src/user/UserAdminController.spec.ts
+++ apps/admin/src/user/UserAdminController.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserAdminController } from './UserAdminController';
 import { UserAdminService } from './UserAdminService';
+import { EntityService } from '@app/entity';
+import { UserEntityRepository } from '@app/entity/user/UserEntityRepository';
 
 describe('AdminController', () => {
   let adminController: UserAdminController;
@@ -8,7 +10,7 @@ describe('AdminController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [UserAdminController],
-      providers: [UserAdminService],
+      providers: [UserAdminService, EntityService, UserEntityRepository],
     }).compile();
 
     adminController = app.get<UserAdminController>(UserAdminController);
---
+++ apps/api/test/unit/UserApiController.spec.ts
@@ -0,0 +1,70 @@
+import { UserApiController } from '../../src/user/UserApiController';
+import { UserApiService } from '../../src/user/UserApiService';
+import { UserEntityRepository } from '@app/entity/user/UserEntityRepository';
+import { User } from '@app/entity/user/User.entity';
+import { instance, mock, when } from 'ts-mockito';
+
+describe('UserApiController', () => {
+  let userApiController: UserApiController;
+
+  beforeEach(async () => {
+    // const app: TestingModule = await Test.createTestingModule({
+    //   controllers: [UserApiController],
+    //   providers: [UserApiService],
+    // }).compile();
+    //
+    // userApiController = app.get<UserApiController>(UserApiController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      userApiController = new UserApiController(
+        new UserApiService(),
+        null,
+        null,
+      );
+      expect(userApiController.getHello()).toBe('Hello World!');
+    });
+
+    it('getUsers', async () => {
+      const id = 1;
+      const user = new User();
+      user.id = id;
+
+      const stubRepository = new (class extends UserEntityRepository {
+        constructor() {
+          super(null);
+        }
+
+        findAll(): Promise<User[]> {
+          return Promise.resolve([user]);
+        }
+      })();
+
+      userApiController = new UserApiController(null, null, stubRepository);
+
+      const users = await userApiController.getUsers();
+      expect(users).toHaveLength(id);
+      expect(users[0].id).toBe(id);
+    });
+
+    it('getUsers with mockito', async () => {
+      const id = 1;
+      const user = new User();
+      user.id = id;
+
+      const stubRepository: UserEntityRepository = mock(UserEntityRepository);
+      when(stubRepository.findAll()).thenResolve([user]);
+
+      userApiController = new UserApiController(
+        null,
+        null,
+        instance(stubRepository),
+      );
+
+      const users = await userApiController.getUsers();
+      expect(users).toHaveLength(id);
+      expect(users[0].id).toBe(id);
+    });
+  });
+});
