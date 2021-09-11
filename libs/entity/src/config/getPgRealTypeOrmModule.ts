88fc2455ee44a3a8532ca5d6f3ba88e03c30e3c8
jojoldu <jojoldu@gmail.com>
2021-09-11 22:08:54 +0900

    real pg 모듈 추가

---
+++ libs/entity/src/config/getPgRealTypeOrmModule.ts
@@ -0,0 +1,16 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+export function getPgTestTypeOrmModule() {
+  return TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'test',
+    password: 'test',
+    database: 'test',
+    entities: [__dirname + '/../**/*.entity.ts'],
+    synchronize: false,
+    namingStrategy: new SnakeNamingStrategy(),
+  });
+}
